7fe3e5c31f56917b9a898365246a6fe85bf0bc97
Vallabh Chugh <vallabh.chugh@sap.com>
2021-11-10 01:26:48 +0530

    HTML,CSS Added from internet

--- website/__init__.py
+++ website/__init__.py
@@ -6,5 +6,11 @@ def create_app():
     app=Flask(__name__)
     #This line is used to encrypt/decreypt session cookies of our web server. This should be confedential in our prod environment
     app.config['SECRET_KEY']='encrypt'
+    #import blueprint for app to use them 
+    from .views import views
+
+    #register a blue print with our flask appliation
+    app.register_blueprint(views,url_prefix='/')
+
 
     return app
---
+++ website/views.py
@@ -0,0 +1,11 @@
+#This helps us to setup a blueprint file for all the URl/routes and we can create actual functions for routes in seperate files for functional programming
+
+from flask import Blueprint
+
+#create a blueprint, try to keep names/variables same as file for ease 
+views=Blueprint('views',__name__)
+
+#create a basic route with our decorator
+@views.route('/')
+def home():
+    return "<h1>Test<h1>."
