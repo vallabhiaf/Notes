ce00d549db0b991140b05bf463ad747fcd133242
Vallabh Chugh <vallabh.chugh@sap.com>
2021-11-11 17:43:53 +0530

    Adding Docs1

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from website import create_app
-'Main file to run the web server for our Notes Web App '
+'Main file to run the web server for our Notes Web App basic'
 app=create_app()
 
 #This line states that we only need to run line 7 if this file is run independently and not imported from another file 
